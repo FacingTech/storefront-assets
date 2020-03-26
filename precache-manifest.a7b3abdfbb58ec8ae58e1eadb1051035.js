b019c2d47a3734250181ec9c4070cb231494c62c
i319701 <eric.wang04@sap.com>
2020-03-26 18:59:48 +0800

    Upload assets

---
+++ precache-manifest.a7b3abdfbb58ec8ae58e1eadb1051035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c11e8dba5b3d2dfee139094345c46c1",
+    "url": "/static/media/rekit-react.5c11e8db.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "481981d438ca98a89c5e",
+    "url": "/static/js/main.481981d4.chunk.js"
+  },
+  {
+    "revision": "7a97aa77f3c01e7b7c2c",
+    "url": "/static/js/1.7a97aa77.chunk.js"
+  },
+  {
+    "revision": "481981d438ca98a89c5e",
+    "url": "/static/css/main.c22c663f.chunk.css"
+  },
+  {
+    "revision": "4d29da6d964dfb5579d13acedba8c29c",
+    "url": "/index.html"
+  }
+];
